e97faa3d00904fc8a08069a65dd608d3af220ed5
joninoni <jonibeltranhayer@gmail.com>
2024-06-13 21:40:05 -0600

    comienzo el  proyecto y añado una validacion basica al formulario

---
+++ src/components/PatientForm.tsx
@@ -0,0 +1,134 @@
+import {SubmitHandler, useForm} from "react-hook-form"
+
+export default function PatientForm() {
+  
+    const {register, handleSubmit,formState : {errors}} = useForm()
+
+    console.log(errors);
+
+    const registerPatients = (data) => {
+        console.log(data);  
+    }
+    
+
+    return (
+        <div className="md:w-1/2 lg:w-2/5 mx-5">
+            <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+    
+            <p className="text-lg mt-5 text-center mb-10">
+                Añade Pacientes y {''}
+                <span className="text-indigo-600 font-bold">Administralos</span>
+            </p>
+    
+            <form 
+                className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"              
+                onSubmit={handleSubmit(registerPatients)}
+            >
+                    <div className="mb-5">
+                        <label htmlFor="name" className="text-sm uppercase font-bold">
+                            Paciente 
+                        </label>
+                        <input  
+                            id="name"
+                            className="w-full p-3  border border-gray-100"  
+                            type="text"
+                            placeholder="Nombre del Paciente"
+                            {...register("nombre",{
+                                required : {
+                                    value : true,
+                                    message : "El campo Nombre es requerido"
+                                }
+                            })}
+                        />
+
+                            {errors.nombre && <p>{errors.nombre.message}</p>}
+                        
+                    </div>
+    
+                    <div className="mb-5">
+                    <label htmlFor="caretaker" className="text-sm uppercase font-bold">
+                        Propietario 
+                    </label>
+                    <input  
+                        id="caretaker"
+                        className="w-full p-3  border border-gray-100"  
+                        type="text" 
+                        placeholder="Nombre del Propietario"
+                        {...register("propietario",{
+                            required : {
+                                value : true,
+                                message : "El campo propietario es requerido"
+                            }
+                        })} 
+                    />
+
+                        {errors.propietario && <p>{errors.propietario.message}</p>}
+                    </div>
+    
+                <div className="mb-5">
+                    <label htmlFor="email" className="text-sm uppercase font-bold">
+                        Email 
+                    </label>
+                    <input  
+                        id="email"
+                        className="w-full p-3  border border-gray-100"  
+                        type="email" 
+                        placeholder="Email de Registro"
+                        {...register("email",{
+                            required : {
+                                value : true,
+                                message : "El campo email es requerido"
+                            }
+                        })} 
+                    />
+
+                        {errors.email && <p>{errors.email.message}</p>}
+                </div>
+    
+                <div className="mb-5">
+                    <label htmlFor="date" className="text-sm uppercase font-bold">
+                        Fecha Alta 
+                    </label>
+                    <input  
+                        id="date"
+                        className="w-full p-3  border border-gray-100"  
+                        type="date"
+                        {...register("fecha",{
+                            required : {
+                                value : true,
+                                message : "El campo fecha es requerido"
+                            }
+                        })}
+                    />
+
+                        {errors.fecha && <p>{errors.fecha.message}</p>}
+                </div>
+                
+                <div className="mb-5">
+                    <label htmlFor="symptoms" className="text-sm uppercase font-bold">
+                    Síntomas 
+                    </label>
+                    <textarea  
+                        id="symptoms"
+                        className="w-full p-3  border border-gray-100"  
+                        placeholder="Síntomas del paciente"
+                        {...register("sintomas",{
+                            required : {
+                                value : true,
+                                message : "El campo sintomas es requerido"
+                            }
+                        })}
+                    ></textarea>
+
+                        {errors.sintomas && <p>{errors.sintomas.message}</p>}
+                </div>
+    
+                <input
+                    type="submit"
+                    className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+                    value='Guardar Paciente'
+                />
+            </form> 
+        </div>
+    )
+  }
